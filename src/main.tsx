23b2eb746ff064265dd824d761d64914ba78bff3
Hrutikkumar <hrutiknaik30@gmail.com>
2026-05-07 17:37:54 +0530

    initial_code

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import './index.css';
+// Since the user requested NO React, we are using index.html as the primary entry.
+// Vite serves index.html at root. We link style.css and script.js in index.html.
+console.log('Static site environment initialized.');
+
